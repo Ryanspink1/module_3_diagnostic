63af79c3f15ea013c0c470303691021d58fc2744
Ryan Spink <ryanspinktech@gmail.com>
2017-03-29 10:04:26 -0600

    working on distance from point right now

--- app/models/Fuel.rb
+++ app/models/Fuel.rb
@@ -1,8 +1,10 @@
 class Fuel < OpenStruct
 
   def self.find_elec(zip_code)
-    DeveloperService.new.developer(zip_code).map do |station|
-      Fuel.new(station)
-    end
+    DeveloperService.new.developer(zip_code)[:fuel_stations]#.map do |stations|
+      #stations.map do |station|
+        #Fuel.new(station)
+      #end
+    #end
   end
 end
