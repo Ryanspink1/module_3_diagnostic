c44ada9592d2ac7b62cdf2f6ba2dc5653d4fa7d2
Ryan Spink <ryanspinktech@gmail.com>
2017-03-29 10:59:53 -0600

    adds feature testing for visiting the root and searching by zip code

--- spec/features/guest_searches_zip_spec.rb
+++ spec/features/guest_searches_zip_spec.rb
@@ -1,4 +1,24 @@
 require 'rails_helper'
 
 feature 'when a guests searches a zip code' do
-  scenario 'they are given a list of fuel stations' do
+  scenario 'they are given a list of the 10 closest fuel stations' do
+    VCR.use_cassette('features/search_by_zip') do
+      visit root_path
+
+        fill_in("q", :with => '80203')
+        click_button("Locate")
+
+      expect(current_path).to eq("/search")
+
+      expect(page).to have_content("UDR")
+      expect(page).to have_content("City of Denver - Cultural Center Complex Garage")
+      expect(page).to have_content("JOULE")
+      expect(page).to have_content("CADACHARGEPOINT")
+      expect(page).to have_content("PUBLIC STATIONS")
+      expect(page).to have_content("City of Denver - Denver Justice Center Garage")
+      expect(page).to have_content("Wells Fargo Center")
+      expect(page).to have_content("Tremont Street Garage")
+      expect(page).to_not have_content("Alabaster")
+    end
+  end
+end
--- spec/models/fuel_spec.rb
+++ spec/models/fuel_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe "Fuel" do
   describe '#master' do
     it 'returns the closest 10 LPG and electric fuel stations within a 6 mile radius of a given zip code' do
-      # VCR.use_cassette('services/master')
+      VCR.use_cassette('services/master') do
         fuel_stations = Fuel.master(80203)
         first = fuel_stations.first
         last  = fuel_stations.last
@@ -16,7 +16,7 @@ describe "Fuel" do
 
         expect(last[:station_name]).to eq("Tremont Street Garage")
         expect(last[:station_name]).to_not eq("City of Denver - Cultural Center Complex Garage")
-      # end
+      end
     end
   end
 end
